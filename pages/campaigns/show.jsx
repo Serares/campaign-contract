9f999bbadae6013d2266cccf290db7a46bbd4d85
Rares <jianu.rares30@gmail.com>
2021-08-22 14:36:17 +0300

    Query returned summary object from campaign contract

--- pages/campaigns/show.jsx
+++ pages/campaigns/show.jsx
@@ -6,9 +6,17 @@ class CampaignShow extends React.Component {
     // called with props as argument to access query params
     static async getInitialProps(props) {
         const campaing = Campaign(props.query.campaignAddress);
+        // {[key: number]: value} it's an object
         const summary = await campaing.methods.getSummary().call();
+
         console.log(summary);
-        return {}
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: sumary[2],
+            approverCount: summary[3],
+            manager: summary[4]
+        }
     }
 
     render() {
